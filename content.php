843212f31bfd6ca5379ffbf0589469e9f1d84a96
cristianfrumusanu <cristian.frumusanu@pixelgrade.com>
2014-08-11 15:42:33 +0300

    post author

--- content.php
+++ content.php
@@ -8,6 +8,15 @@
 		<?php the_post_thumbnail(); ?>
 	</div>
 	<div class="book-info">
-		<?php the_title( sprintf( '<h1 class="book-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a><span class="book-author">by Author</span></h1>' ); ?>
+		<?php
+			$post_meta = get_post_custom(get_the_ID());
+
+			$post_author = '';
+
+			if(!empty($post_meta['book_author'])){
+				$post_author = 'by ' . $post_meta['book_author'][0];
+			}
+		?>
+		<?php the_title( sprintf( '<h1 class="book-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a><span class="book-author">' . $post_author . '</span></h1>' ); ?>
 	</div>
 </article>
